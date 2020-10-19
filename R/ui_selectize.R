1b125989677166ea52e4e764777b9683e027f24b
Dewi-Amaliah <dama0007@student.monash.edu>
2020-10-20 00:21:04 +1100

    Add ui_selectize function

---
+++ R/ui_selectize.R
@@ -0,0 +1,19 @@
+#' Refactor User Interface selectizeInput Function
+#' 
+#' This function aim to simplify the repetitive call of shiny's selectizeInput function. 
+#' It will be called in app.R file.
+#' 
+#' @param id Input id that will be used in the server side.
+#' @param choices List of value to select. 
+#' 
+#' @examples 
+#' ui_selectize("country_select", c("Italy", "Spain", "France", "Austria", "United Kingdom"))
+#' 
+#' @export
+ui_selectize <- function(id, choices){
+  shiny::selectizeInput(id, 
+                 label = "Step 1: Select or type at least 1 country",
+                 choices,
+                 selected = c("Italy", "Austria", "Spain"),
+                 multiple = TRUE)
+}
