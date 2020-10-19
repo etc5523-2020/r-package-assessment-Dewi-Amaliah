579aa502159312aceae16ef42a992ec4ec5e2910
Dewi-Amaliah <dama0007@student.monash.edu>
2020-10-19 13:04:47 +1100

    Add country centroids data

---
+++ R/country_centroids.R
@@ -0,0 +1,13 @@
+#' The centroid coordinate of countries in the world
+#' 
+#' A dataset contains centroid coordinate of countries in the world. 
+#' 
+#' @format a tibble with 241 observations and 3 variables.
+#' 
+#' - **name_long**: name of the country
+#' - **Longitude**: longitude of country's centroid
+#' - **Latitude**: latitude of country's centroid
+#' 
+#' @source [Harvard World Map](https://worldmap.harvard.edu/data/geonode:country_centroids_az8)
+#' 
+"country_centroids"
---
+++ data-raw/country_centroids.R
@@ -0,0 +1,9 @@
+## code to prepare `country_centroids` dataset goes here
+
+library(tidyverse)
+country_centroids <- read_csv("data-raw/country_centroids.csv") %>%
+  select(name_long, 
+         Longitude,
+         Latitude)
+
+usethis::use_data(country_centroids, overwrite = TRUE)
